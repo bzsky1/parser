d3207ab7586fbbfdcd86e38282d7caab662ef655
Bohdan <97465888+bzsky1@users.noreply.github.com>
2022-06-09 13:03:30 +0200

    Add files via upload

--- prs.py
+++ prs.py
@@ -3,13 +3,15 @@ import csv
 from bs4 import BeautifulSoup as BS
 from time import sleep
 
-CSV = 'posts.csv'
+
 HOST = 'https://scrapingclub.com'
 URL = 'https://scrapingclub.com/exercise/list_basic/?page=1'
 HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 }
+
+file_name = input('File name where to save info: ') + '.csv'
 
 def get_html(url, params=''):
     r = requests.get(url, headers=HEADERS, params=params)
@@ -41,17 +43,22 @@ def save_in_doc(items, path):
 def how_many_pages_parser():
     PAGINATION = input("How many pages You want me to pars?: ")
     PAGINATION = int(PAGINATION.strip())
+
+    sleeping_time = input('Enter pause between pages in seconds: ')
+    sleeping_time = int(sleeping_time.strip())
+
     html = get_html(URL)
     if html.status_code == 200:
         posts = []
         for page in range(1, PAGINATION + 1):
 
-            sleep(4) # Slowing down the process to hide that it is not a human
+            # Slowing down the process to hide that it is not a human
+            sleep(sleeping_time)
 
             print(f'Parsing of page № {page}')
             html = get_html(URL, params={'page': page})
             posts.extend(get_content(html.text))
-            save_in_doc(posts, CSV)
+            save_in_doc(posts, file_name)
     else:
         print('Error :(')
 
